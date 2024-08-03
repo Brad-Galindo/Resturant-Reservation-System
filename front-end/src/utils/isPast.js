fe6ebd692fbdffbd2754d6f0e7d08071c56184cd
brad-galindo <mduck9419@gmail.com>
2024-08-02 19:05:45 -0500

    .

--- front-end/src/utils/isPast.js
+++ front-end/src/utils/isPast.js
@@ -1,23 +1,23 @@
 function isPast(dateTimeString) {
-    const now = new Date();
-  
-    // Parse the date string manually
-    const [datePart, timePart] = dateTimeString.split('T');
-    const [year, month, day] = datePart.split('-').map(Number);
-    const [hour, minute] = timePart ? timePart.split(':').map(Number) : [0, 0];
-  
-    // Create a date object in local time
-    const dateToCheck = new Date(year, month - 1, day, hour, minute);
-  
-    console.log('Input string:', dateTimeString);
-    console.log('Now (Local):', now.toString());
-    console.log('Date to check (Local):', dateToCheck.toString());
-    console.log('Is past?', dateToCheck < now);
-  
-    return dateToCheck < now;
-  }
-  
+  const now = new Date();
 
-  
-  module.exports = isPast ;
-  
+  // Parse the date string manually and treat it as UTC
+  const [datePart, timePart] = dateTimeString.split('T');
+  const [year, month, day] = datePart.split('-').map(Number);
+  const [hour, minute] = timePart ? timePart.split(':').map(Number) : [0, 0];
+
+  // Create a date object in UTC
+  const dateToCheck = new Date(Date.UTC(year, month - 1, day, hour, minute));
+
+  // Get the current time in UTC
+  const nowUtc = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds()));
+
+  console.log('Input string (UTC):', dateTimeString);
+  console.log('Now (UTC):', nowUtc.toISOString());
+  console.log('Date to check (UTC):', dateToCheck.toISOString());
+  console.log('Is past?', dateToCheck < nowUtc);
+
+  return dateToCheck < nowUtc;
+}
+
+module.exports = isPast;
